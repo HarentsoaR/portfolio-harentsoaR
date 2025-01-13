6121fc7812aba08b984bee1e4d63de05b6c5305e
HarentsoaR <randriamaholimanana1@gmail.com>
2025-01-13 12:33:22 +0300

    Loader off 2.0

--- src/app/[locale]/career/page.tsx
+++ src/app/[locale]/career/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { Suspense } from 'react';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
-import { Loader } from '@/components/Loader';
 import { useTranslations } from 'next-intl';
 
 const educationData = [
